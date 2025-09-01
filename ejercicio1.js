91f2f66226b6ef35d6f117750abf72dfdb9f7d49
Flor <69876027+fserrano97@users.noreply.github.com>
2025-09-01 18:44:52 -0300

    Delete ejercicio1.js

--- ejercicio1.js
+++
@@ -1,6 +0,0 @@
-let num ;
-let total;
-
-num = Number(prompt("Ingrese un número cualquiera")); // pedimos el número al usuario
-total = num * 2; // multiplicamos por 2
-console.log("El resultado es:", total);
