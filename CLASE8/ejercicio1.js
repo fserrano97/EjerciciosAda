26874bddb9b99972ab1ef03922b96adacf26b959
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-01 19:20:53 -0300

    Agrego ejercicio2.js y .gitignore

---
+++ CLASE8/ejercicio1.js
@@ -0,0 +1,6 @@
+let num ;
+let total;
+
+num = Number(prompt("Ingrese un número cualquiera")); // pedimos el número al usuario
+total = num * 2; // multiplicamos por 2
+console.log("El resultado es:", total);
