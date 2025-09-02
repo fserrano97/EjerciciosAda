33740a3c9003530fe1687de229d655c12e88fd52
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-01 21:49:21 -0300

    Ejercicio n5 de cadena de texto primer formula

---
+++ CLASE8/ejercicio5.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')();
+
+let nombre;
+let saludo = 'Hola';
+let saludo2 = 'Como te va?';
+
+nombre = prompt('Ingrese su nombre o apodo: ');
+
+console.log(saludo+' '+nombre+' '+saludo2);
+
+
+
