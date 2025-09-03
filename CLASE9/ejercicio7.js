7a315122d85df63461a89e553165796790b5a369
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-02 21:48:15 -0300

    Agrego ejercicio 7 de clase 9

---
+++ CLASE9/ejercicio7.js
@@ -0,0 +1,16 @@
+   // Pide al usuario que ingrese su peso en kilogramos y conviértelo a 
+   // libras. Muestra el resultado con un mensaje. (Averigua como pasar de 
+   // kg a libras, Pista: 2.20462)   
+
+const prompt = require('prompt-sync')();
+
+function Convertidor(peso) {
+    const libras = 2.20462;
+    return peso * libras;
+}
+
+let peso = Number(prompt('Ingrese su peso en kg: '));
+let pesoEnLibra = Convertidor(peso);
+
+// Usando interpolación de strings
+console.log(`${peso} kg son ${pesoEnLibra.toFixed(2)} libras.`);
