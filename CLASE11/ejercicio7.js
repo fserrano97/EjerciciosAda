85277a102597249ef8b782f176396ebc8bba199f
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-04 15:31:44 -0300

    Agrego ejercicio7 de clase 11

---
+++ CLASE11/ejercicio7.js
@@ -0,0 +1,34 @@
+
+const prompt = require('prompt-sync')();
+
+let num = Number(prompt('INGRESE UN NUMERO: '));
+let num2 = Number(prompt('INGRESE OTRO NUMERO: '));
+let oper = prompt('INGRESE OPERANDO(suma,resta,dividir,multiplicar): ');
+let total;
+
+switch (oper){
+    case 'suma':
+        total = num + num2
+        console.log('Total= ',total);
+        break;
+    case 'resta':
+        total=num - num2
+        console.log('Total= ',total);
+        break;
+    case 'dividir':
+        if (num2 === 0) {
+            console.log('ERROR. No se puede dividir entre 0.');
+        } else {
+            total = num / num2;
+            console.log('Total= ',total);
+        }
+        total = num / num2
+        console.log('Total= ',total);
+        break;
+    case 'multiplicar':
+        total = num*num2
+        console.log('Total= ',total);
+        break;
+    default:
+    console.log('Operacion invalidad. ERROR! Debes ingresar suma, resta, dividir o multiplicar..');
+}
