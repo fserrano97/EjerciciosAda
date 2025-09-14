559e44c7945604b9e2ba3871f2069631ff41648d
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-14 11:54:56 -0300

    Ejercicio 6 horas a minutos

--- CLASE13/ej6.js
+++ CLASE13/ej6.js
@@ -1,6 +1,10 @@
 
 
 function convertirHorasAMinutos(horas){
+    return horas*60;
 
-    
-}
+};
+
+let Cant_horas = parseInt(prompt('Ingrese cantidad de horas: '));
+
+console.log('La cantidad de ' ,Cant_horas, ' horas es de : ',convertirHorasAMinutos(Cant_horas), ' minutos.');
