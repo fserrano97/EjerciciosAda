f91be650d33f56d8db4e04845dd1d573ea8b6309
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-14 22:37:20 -0300

    ejercicio7

---
+++ CLASE15/ejer7.js
@@ -0,0 +1,8 @@
+
+let comidas =["Pizza", "Hamburguesa", "Tacos"];
+
+if ((comidas.indexOf("Sushi")) === -1){
+    console.log('Sushi no esta en la comida!');
+}else
+    console.log('Tenemos SUSHIII');
+
