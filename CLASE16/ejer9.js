5bd4dfb60e261929e02280bd1479e117146c2d50
fserrano97-pro <florenciaayelenserrano@gmail.com>
2025-09-16 16:20:58 -0300

    ejer9

---
+++ CLASE16/ejer9.js
@@ -0,0 +1,10 @@
+//Imagina que tienes una lista de palabras ['sol', 'luna', 'estrella']. Queremos 
+//saber cuántas letras hay en total entre todas las palabras. Por ejemplo, 'sol' 
+//tiene 3 letras, 'luna' tiene 4, y 'estrella' tiene 8. La suma total debería ser 15.
+
+
+let nombres = ['sol', 'luna', 'estrella'];
+
+let total = nombres.reduce((acum, nombre) => acum + nombre.length, 0);
+
+console.log(total);
